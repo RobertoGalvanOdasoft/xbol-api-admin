291fb550f047661f912c2540ad62c6e10058b49f
Daniel Medina <isc.daniel.medina@gmail.com>
2026-01-12 14:40:11 -0800

    feat(seats.io): add booking controller

---
+++ Odasoft.XBOL.AdminAPI/Odasoft.XBOL.AdminAPI/Controllers/BookingsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Odasoft.XBOL.AdminAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingsController(ITicketingClient ticketingClient) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<BookingResult>> CreateBooking([FromBody] BookingRequest request)
+        {
+            var result = await ticketingClient.BookingAsync(request);
+
+            return Ok(new BookingResult { Message = "Booking created successfully", Tickets = result });
+        }
+
+        // TODO: Move to an appropiate location
+        public class BookingResult
+        {
+            public long? BookingId { get; set; }
+            public string Message { get; set; }
+            public IEnumerable<string> Tickets { get; set; } = [];
+        }
+    }
+}

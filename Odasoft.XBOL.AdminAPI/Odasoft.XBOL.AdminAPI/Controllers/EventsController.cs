64c48077e7b482e2c19db9b00e2195a891d7a890
RobertoGalvan <roberto.galvan@odasoft.com.mx>
2026-01-15 10:28:35 -0800

    fix(events): Remove unused endpoint

--- Odasoft.XBOL.AdminAPI/Odasoft.XBOL.AdminAPI/Controllers/EventsController.cs
+++ Odasoft.XBOL.AdminAPI/Odasoft.XBOL.AdminAPI/Controllers/EventsController.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using Odasoft.XBOL.Business.Services;
-using Odasoft.XBOL.Models;
 
 namespace Odasoft.XBOL.AdminAPI.Controllers
 {
@@ -15,21 +14,6 @@ namespace Odasoft.XBOL.AdminAPI.Controllers
         {
             _eventService = eventService;
             _ticketingClient = ticketingClient;
-        }
-
-        /// <summary>
-        /// Retrieves the details of a specific event by its unique identifier.
-        /// </summary>
-        /// <param name="eventId">The unique identifier of the event to retrieve. Must be a valid event ID.</param>
-        /// <returns>An <see cref="ActionResult{Event}"/> containing the event details if found; otherwise, a response indicating
-        /// that the event does not exist.</returns>
-        [HttpGet("{eventId}")]
-        public async Task<ActionResult<Event>> GetEventDetails([FromRoute] long eventId)
-        {
-            var eventDetails = await _eventService.GetEventByIdAsync(eventId);
-
-            // TODO: Set the proper Response DTO
-            return Ok(eventDetails);
         }
 
         [HttpGet]
